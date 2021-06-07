f12c6d921b49a2e4a69d9c80d7a77b3ef3636568
Smit Ray <humancoder@outlook.com>
2021-06-08 02:24:38 +0530

    feat(integrated apollo server): apollo server implementation

--- server/server.ts
+++ server/server.ts
@@ -1,22 +1,51 @@
+import 'reflect-metadata';
 import Koa from 'koa';
 import Router from '@koa/router';
 import cors from '@koa/cors';
 import nextApp from 'next';
 import loggerMain from 'tracer';
+import { ApolloServer, gql } from 'apollo-server-koa';
 
 export const logger = loggerMain.colorConsole();
+
+const Query = gql`
+  type Query {
+    _empty: String
+  }
+
+  schema {
+    query: Query
+  }
+`;
 
 export default class Server {
   server: Koa;
 
   async init(): Promise<void> {
     this.server = new Koa();
+    await this.setupGraphQl();
     this.serverSetup();
     this.nextSetup();
   }
 
   serverSetup(): void {
     this.server.use(cors());
+  }
+
+  async setupGraphQl(): Promise<void> {
+    try {
+      const apolloServer = new ApolloServer({
+        typeDefs: [Query],
+        playground: process.env.NODE_ENV !== 'production',
+      });
+
+      apolloServer.applyMiddleware({
+        app: this.server,
+        path: '/api/graphql',
+      });
+    } catch (error) {
+      logger.error(error);
+    }
   }
 
   async nextSetup(): Promise<void> {
